315b8032081df40a1d1089feccf30e7e6c355cef
anpyd <anpyd@outlook.com>
2024-05-18 20:41:26 +0800

    update: add func <do_task> and commands "stop-trigger" "wake-trigger"

--- adcm.py
+++ adcm.py
@@ -78,6 +78,7 @@ class ADCM_Trigger(object):
 adcm_project_name: str = None
 adcm_project_tasks: dict = {}
 adcm_project_triggers: dict = {}
+adcm_project_trigger_signs: dict = {}
 adcm_project_trigger_threads: dict = {}
 
 """ create a new project """
@@ -125,12 +126,21 @@ def add_trigger(
     
     adcm_project_triggers[name] = ADCM_Trigger(dependence, tasks)
     
+""" do specific task """
+def do_task(name: str) -> None:
+    global adcm_project_tasks
+    if name not in adcm_project_tasks:
+        raise RuntimeError("Failed to call <do_task> as task \"{0}\" is not existed!".format(name))
+    
+    adcm_project_tasks[name].exec()
 
-""" launch adcm manager """
-def launch() -> None:
+
+""" internal launcher """
+def _adcm_launch() -> None:
     global adcm_project_name
     global adcm_project_tasks
     global adcm_project_triggers
+    global adcm_project_trigger_signs
     global adcm_project_trigger_threads
 
     # check workflow integrity
@@ -140,25 +150,27 @@ def launch() -> None:
         raise RuntimeError("Failed to launch adcm as project's task-list is empty!")
 
     # launch triggers
-    stop_sign = threading.Event()
     for index in adcm_project_triggers:
-        trigger_thread = threading.Thread(target=adcm_project_triggers[index].run, args=(stop_sign, ))
+        thread_sign = threading.Event()
+        trigger_thread = threading.Thread(target=adcm_project_triggers[index].run, args=(thread_sign, ))
         adcm_project_trigger_threads[index] = trigger_thread
+        adcm_project_trigger_signs[index] = thread_sign
         trigger_thread.start()
 
     # create console
     while True:
-        cmd = str(input("\033[1;32madcm#{0} >\033[m ".format(adcm_project_name)))
+        cmd = str(input("\033[1;32madcm@{0} >\033[m ".format(adcm_project_name)))
 
         # empty command
         if len(cmd.replace(" ", "")) == 0:
             continue
 
         else:
-            if cmd == "exit":
+            scmd = cmd.split(' ') # split input command
+            if scmd[0] == "exit":
                 break
 
-            elif cmd == "clear":
+            elif scmd[0] == "clear":
                 if os.name == "nt":
                     # clear command for Windows CMD
                     os.system("cls")
@@ -166,11 +178,12 @@ def launch() -> None:
                     # clear command for Linux and macOS
                     os.system("clear")
 
-            elif cmd == "task-list":
+            elif scmd[0] == "task-list":
+                print("| ", end='')
                 col_num = 1
                 for index in adcm_project_tasks:
-                    if col_num == 6: # NOTICE: max number of displayed names in one row is 5
-                        print("") # enter to next line
+                    if col_num == 11: # NOTICE: max number of displayed names in one row is 10
+                        print("\n| ", end='') # enter to next line
                         col_num = 1
                     else:
                         col_num += 1
@@ -179,16 +192,65 @@ def launch() -> None:
 
                 print("") # enter to next line
 
-            elif cmd == "trigger-list":
+            elif scmd[0] == "trigger-list":
                 for index in adcm_project_trigger_threads:
                     print("| {0} | status: {1}".format(index, "active" if adcm_project_trigger_threads[index].is_alive() else "stopped"))
 
-            # not built-in command, run task
+            elif scmd[0] == "stop-trigger":
+                if len(scmd) == 1:
+                    print("usage: stop-trigger <trigger-name>")
+                    continue
+
+                trigger_name = scmd[1]
+
+                if trigger_name not in adcm_project_triggers:
+                    print("error: failed to stop trigger \"{0}\" as it doesn't exist!".format(trigger_name))
+                    continue
+
+                if adcm_project_trigger_threads[trigger_name].is_alive():
+                    adcm_project_trigger_signs[trigger_name].set()
+                    time.sleep(1) # wait for 1s to stop thread
+                    print("info: successfully stop trigger!" if not adcm_project_trigger_threads[trigger_name].is_alive() else "error: failed to stop trigger!")
+                else:
+                    print("trigger \"{0}\" is already stopped!".format(trigger_name))
+
+            elif scmd[0] == "wake-trigger":
+                if len(scmd) == 1:
+                    print("usage: wake-trigger <trigger-name>")
+                    continue
+
+                trigger_name = scmd[1]
+
+                if trigger_name not in adcm_project_triggers:
+                    print("error: failed to stop trigger \"{0}\" as it doesn't exist!".format(trigger_name))
+                    continue
+
+                if not adcm_project_trigger_threads[trigger_name].is_alive():
+                    thread_sign = adcm_project_trigger_signs[trigger_name]
+                    thread_sign.clear()
+                    adcm_project_trigger_threads[trigger_name] = threading.Thread(target=adcm_project_triggers[trigger_name].run, args=(thread_sign, ))
+                    adcm_project_trigger_threads[trigger_name].start()
+                    time.sleep(1) # wait for 1s to wake thread
+                    print("info: successfully wake trigger!" if adcm_project_trigger_threads[trigger_name].is_alive() else "error: failed to wake trigger!")
+                else:
+                    print("trigger \"{0}\" is already active!".format(trigger_name))
+
+            # not built-in command, consider task
             else:
-                if cmd in adcm_project_tasks:
+                if scmd[0] in adcm_project_tasks:
                     adcm_project_tasks[cmd].exec()
                 else:
                     print("\033[1;32;31merror: task \"{0}\" is not existed!\033[m ".format(cmd))
 
-    # exit adcm after collecting threads
-    stop_sign.set()
+
+""" launch the adcm manager """
+def launch() -> None:
+    try:
+        _adcm_launch()
+    except RuntimeError as error_info:
+        print("RuntimeError occurred while running! Exit...")
+        print("> details: {0}".format(error_info))
+    finally:
+        # stop all thread before exit
+        for index in adcm_project_trigger_signs:
+            adcm_project_trigger_signs[index].set()
